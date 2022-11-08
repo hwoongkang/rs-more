206f5e371c10bd1beeb61d402df8eb0f8957a1a8
Kang Hyunwoong <hyunwoong.kang@leedotype.com>
2022-11-08 17:02:10 +0700

    TODO 한 번 간단히 정리, file handle 실습

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let no_prob = fs::File::open("./test_dir/short_text.txt").expect("you will not see this error");
+    println!("no_prob file handle: {:?}", no_prob);
+    let is_dir = fs::File::open("./test_dir/dir").expect("Is a directory");
+    println!("is_dir file handle: {:?}", is_dir);
+    let mut str = String::new();
+    str = fs::read_to_string("./test_dir/dir").expect("Is a directory");
+    println!("str: {:?}", str);
 }
