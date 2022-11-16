d57fcb822c43a96933b701fe6454b6c13aa264a1
Hyunwoong Kang <hyunwoong.kang@leedotype.com>
2022-11-16 20:57:44 +0900

    decided to use crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
+use crossterm::event;
+use crossterm::terminal;
 use std::fs;
-use std::io;
 
 fn main() {
-    let no_prob = fs::File::open("./test_dir/short_text.txt").expect("you will not see this error");
-    println!("no_prob file handle: {:?}", no_prob);
-    let is_dir = fs::File::open("./test_dir/dir").expect("Is a directory");
-    println!("is_dir file handle: {:?}", is_dir);
-    let mut str = String::new();
-    str = fs::read_to_string("./test_dir/dir").expect("Is a directory");
-    println!("str: {:?}", str);
+    let bible = fs::File::open("./test_dir/holy_bible.txt").expect("Failed to open file");
+
+    println!("{:?}", terminal::size());
+    println!("{:?}", terminal::is_raw_mode_enabled());
+    terminal::enable_raw_mode().expect("Could not enable raw mode");
+
+    loop {
+        match event::read().expect("Failed to read event") {
+            event::Event::Key(event) => match event.code {
+                event::KeyCode::Char('c') => {
+                    if event.modifiers == event::KeyModifiers::CONTROL {
+                        break;
+                    }
+                }
+                event::KeyCode::Up => {
+                    println!("Up");
+                }
+                _ => {
+                    println!("Don't care");
+                }
+            },
+            _ => {
+                println!("don't care")
+            }
+        }
+    }
+    terminal::disable_raw_mode().expect("Could not disable raw mode");
 }
